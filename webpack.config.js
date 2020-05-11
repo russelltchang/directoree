74a708a416fc3335dc222fd8f7eaa8f29b806f98
Russell <Russell@Russells-MacBook-Pro.local>
2020-05-11 15:26:53 -0700

    add webpack presets

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const dotenv = require('dotenv');
 const webpack = require('webpack');
-
-// const env = dotenv.config().parsed;
-  
-// // reduce it to a nice object, the same as before
-// const envKeys = Object.keys(env).reduce((prev, next) => {
-//   prev[`process.env.${next}`] = JSON.stringify(env[next]);
-//   return prev;
-// }, {});
 
 module.exports = {
   module: {
@@ -18,7 +9,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
         }
       },
       {
