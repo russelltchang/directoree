eeb669b0f1f6fe699e321614816a62f19751dc63
Russell <Russell@Russells-MacBook-Pro.local>
2020-05-11 17:40:37 -0700

    run a build

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "./dist/index.html"));
 });
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(port, ()=>{
     console.log('Listening at ' + port);
 });
