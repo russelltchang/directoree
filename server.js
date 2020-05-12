fc5673888f54b57083eceaa62a4b312e25eeb324
Russell <Russell@Russells-MacBook-Pro.local>
2020-05-11 18:04:11 -0700

    add another home route

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/dist')); //makes dist the root for anything that index.html requires
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./dist/index.html"));
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "./dist/index.html"));
 });
