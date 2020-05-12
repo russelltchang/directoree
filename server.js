ce55b9c2a8e69fd54f41663af1e340796a03f160
Russell <Russell@Russells-MacBook-Pro.local>
2020-05-11 17:35:48 -0700

    server change

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var axios = require('axios')
 var cors = require('cors');
 var bodyParser = require('body-parser');
-var bcrypt = require('bcrypt');
 var session = require('express-session');
 var port = process.env.PORT || 3000; 
 ;
@@ -18,6 +16,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "./dist/index.html"));
 });
 
-app.listen(port, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Listening at ' + port);
 });
