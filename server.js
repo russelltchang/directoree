5fdc0d0d992a7538a1261a91edeb868e5c7297ee
Russell <Russell@Russells-MacBook-Pro.local>
2020-05-17 16:13:29 -0700

    package.json edits

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var cors = require('cors');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000; 
 
 app.use(cors({credentials: true, origin: 'http://localhost:8080'})); // cross port cookie when test
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/dist')); //makes dist the root for anything that index.html requires
+//makes dist the root for anything that index.html requires
+app.use(express.static(__dirname + '/dist')); 
+
+//change to mLab on production
+// mongoose.connect(url, {useNewUrlParser: true, useCreateIndex: true}).then(()=>console.log('DB Connected!'));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "./dist/index.html"));
